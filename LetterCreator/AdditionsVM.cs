f571111c99511a6e4dfd156117320c94e85c272f
Gesporidgers <49988107+Gesporidgers@users.noreply.github.com>
2025-04-20 19:36:10 +0300

    Добавлен BoolInverter, команды и логика навигации
    - Добавлен ресурс `BoolInverter` в XAML для управления видимостью.
    - Изменена структура интерфейса: добавлены отступы, кнопки и привязки.
    - Добавлен класс `BoolInverter` для инверсии булевых значений.
    - В `AdditionsVM.cs` добавлены команды `Prev`, `Next`, `SaveAll` и свойство `PageIndex`.
    - Реализована работа с файлами: сериализация/десериализация `additions.json`.
    - Обновлена логика навигации и проверки возможности перехода между страницами.
    - Привязки в XAML обновлены для использования новых команд и `BoolInverter`.

--- LetterCreator/AdditionsVM.cs
+++ LetterCreator/AdditionsVM.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace LetterCreator
@@ -14,22 +16,66 @@ namespace LetterCreator
 		public List<string> additions;
 		[Property] string _text;
 		[Property] string _navText;
-		private int index = 0;
+		[Property]private int pageIndex = 0;
 
 		[Command]
 		private void Add()
 		{
-			this.EmptyElements =false;
+			this.EmptyElements = false;
 			additions.Add(this._text);
-			index++;
-			this.NavText = $"{index}/{additions.Count}";
+			PageIndex++;
+			this.NavText = $"{pageIndex}/{additions.Count}";
 		}
-		
+		[Command]
+		private void Next()
+		{
+			additions[pageIndex - 1] = this.Text;
+			if (pageIndex == additions.Count)
+			{
+				additions.Add(string.Empty);
+				this.Text = string.Empty;
+				PageIndex++;
+				this.NavText = $"{pageIndex}/{additions.Count}";
+			}
+			else
+			{
+				this.Text = additions[pageIndex];
+				PageIndex++;
+				this.NavText = $"{pageIndex}/{additions.Count}";
+			}
+		}
+
+		[Command(CanExecuteMethod = nameof(CanPrev))]
+		private void Prev()
+		{
+			additions[pageIndex - 1] = this.Text;
+			this.Text = additions[pageIndex - 2];
+			PageIndex--;
+			this.NavText = $"{pageIndex}/{additions.Count}";
+		}
+
+		[CommandInvalidate(nameof(PageIndex))]
+		private bool CanPrev() => pageIndex - 1 > 0;
+
 		partial void OnInitialize()
 		{
 			emptyElements = true;
 			additions = new List<string>();
 			_text = string.Empty;
+			if (File.Exists(FileSystem.Current.AppDataDirectory + "\\additions.json"))
+			{
+				additions = JsonSerializer.Deserialize<List<string>>(File.ReadAllText("additions.json"));
+				PageIndex = 1;
+				this.NavText = $"{pageIndex}/{additions.Count}";
+				this.Text = additions[pageIndex - 1];
+			}
+		}
+
+		[Command]
+		private void SaveAll()
+		{
+			additions[pageIndex - 1] = this.Text;
+			File.WriteAllText(FileSystem.Current.AppDataDirectory+"\\additions.json", JsonSerializer.Serialize(additions));
 		}
 	}
 }
